bed120f5ad02dff5252ea9fd5cb662542bf89ac4
Subashini <subashinis2017@gmail.com>
2019-10-09 20:39:38 +0000

    Add task creation to the API

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,12 @@ class TasksController < ApplicationController
         render json: task
     end
 
+    def create
+        task = Task.create(task_params)
+        render json: task
+    end
+    
+
     private 
 
     def task_params
